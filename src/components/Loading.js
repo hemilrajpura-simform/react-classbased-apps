2a58adfe12a78aa18c914eaf8b731c9033231cc9
hemilrajpura-simform <hemil.r@simformsolutions.com>
2022-05-13 11:05:39 +0530

    converted into class based components

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,13 +2,16 @@ import React from 'react'
 import './UserList.css';
 import { Loader } from 'react-feather';
 
-
-const Loading = (props) => {
-  
-    return (
+class Loading extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
         <div className='PageNotFound' id="Loading">
             <h1>Loading <Loader className='RefreshCw' /> </h1>
         </div>
     )
+}
 }
 export default Loading;
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,40 +3,84 @@ import React from 'react';
 import './UserList';
 
 
-const Popup = (props) => {
+class Popup extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
 
-    var MonthlyClicks = Math.floor(Math.random() * 10000);
-    var clicksReviewed = Math.floor(Math.random() * 10000);
-    var planUses = Math.floor(Math.random() * 100);
-    const planUsesStyle = {
-        width: planUses + '%'
+    MonthlyClicks = Math.floor(Math.random() * 10000);
+    clicksReviewed = Math.floor(Math.random() * 10000);
+    planUses = Math.floor(Math.random() * 100);
+    planUsesStyle = {
+        width: this.planUses + '%'
     };
+    
+    redner() {
+        return (
+            <div>
 
-    return (
-        <div>
+                <img src={this.props.selectedUser.image} alt='' />
+                <h4>{this.props.selectedUser.username} </h4>
+                <p>{this.props.selectedUser.email}</p>
+                <h3>Your Plan: Standard</h3>
+                <button>Active User</button>
+                <h5>Plan Uses</h5>
+                <div className='plan-uses-line' style={this.planUsesStyle}> </div>
 
-            <img src={props.selectedUser.image} alt='' />
-            <h4>{props.selectedUser.username} </h4>
-            <p>{props.selectedUser.email}</p>
-            <h3>Your Plan: Standard</h3>
-            <button>Active User</button>
-            <h5>Plan Uses</h5>
-            <div className='plan-uses-line' style={planUsesStyle}> </div>
-      
-            <div className='statistic-main'>
-                <div className='reviewed'>
-                    <h2>{clicksReviewed}</h2>
-                    <p>clicks reviewed</p>
+                <div className='statistic-main'>
+                    <div className='reviewed'>
+                        <h2>{this.clicksReviewed}</h2>
+                        <p>clicks reviewed</p>
+                    </div>
+                    <div className='statistic-line'></div>
+                    <div className='clicks'>
+                        <h2>{this.MonthlyClicks}</h2>
+                        <p>Monthly clicks</p>
+                    </div>
                 </div>
-                <div className='statistic-line'></div>
-                <div className='clicks'>
-                    <h2>{MonthlyClicks}</h2>
-                    <p>Monthly clicks</p>
-                </div>
-            </div>
-
-        </div>
-    );
+            </div >
+        )
+    }
 };
 export default Popup;
+
+
+
+// const Popup = (props) => {
+
+
+//     var MonthlyClicks = Math.floor(Math.random() * 10000);
+//     var clicksReviewed = Math.floor(Math.random() * 10000);
+//     var planUses = Math.floor(Math.random() * 100);
+//     const planUsesStyle = {
+//         width: planUses + '%'
+//     };
+
+//     return (
+//         <div>
+
+//             <img src={props.selectedUser.image} alt='' />
+//             <h4>{props.selectedUser.username} </h4>
+//             <p>{props.selectedUser.email}</p>
+//             <h3>Your Plan: Standard</h3>
+//             <button>Active User</button>
+//             <h5>Plan Uses</h5>
+//             <div className='plan-uses-line' style={planUsesStyle}> </div>
+      
+//             <div className='statistic-main'>
+//                 <div className='reviewed'>
+//                     <h2>{clicksReviewed}</h2>
+//                     <p>clicks reviewed</p>
+//                 </div>
+//                 <div className='statistic-line'></div>
+//                 <div className='clicks'>
+//                     <h2>{MonthlyClicks}</h2>
+//                     <p>Monthly clicks</p>
+//                 </div>
+//             </div>
+
+//         </div>
+//     );
+// };
+// export default Popup;
